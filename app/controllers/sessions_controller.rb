6cc89f81613bb1a88e5e3079b6f36398f14c15b7
Mario Avila <avilamario16@gmail.com>
2015-11-04 21:27:29 -0600

    Added new views and style befor login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,17 +2,16 @@ class SessionsController < ApplicationController
   def new
   end
 
-
-    def create
-      user = User.find_by(user: params[:session][:user])
-      if user && user.authenticate(params[:session][:password])
-        log_in user
-        redirect_to user
-      else
-        flash.now[:danger] = 'Invalid email/password combination'
-        render 'new'
-      end
+  def create
+    user = User.find_by(user: params[:session][:user])
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to :aboutus
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
     end
+  end
 
   def destroy
     log_out
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,18 @@
 class WelcomeController < ApplicationController
   def index
-    
+
   end
+  def home
+  end
+
+  def aboutus
+  end
+
+  def project
+  end
+
+  def hello
+    @random = Place.order("RANDOM()").limit(3)
+  end
+
 end
