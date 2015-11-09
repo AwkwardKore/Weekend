895d05a92e6e6ec424d40c333e35eeea920c1381
Daniel <dani.islas96@gmail.com>
2015-11-08 21:29:19 -0600

    Add feedback for favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,7 +2,7 @@ class FavoritesController < ApplicationController
   def create
     @favorite = current_user.favorite_places.new(favorite_params)
     if @favorite.save
-       redirect_to :back, notice: "You favorited #{Place.find(params[:id]).name}"
+       redirect_to(:back, alert: "Se agregó a favoritos: #{Place.find(params[:id]).name}")
     else
       @place = Place.find(params[:id])
       flash.now[:error] = "There was an error."
@@ -12,7 +12,7 @@ class FavoritesController < ApplicationController
 
   def destroy
     if current_user.favorite_places.find(params[:favorite_id]).destroy
-      redirect_to place_path(params[:id]), notice: "You unfavorited #{Place.find(params[:id]).name}"
+      redirect_to(place_path(params[:id]), alert: "Se quitó de favoritos: #{Place.find(params[:id]).name}")
     else
       @place = Place.find(params[:id])
       flash.now[:error] = "There was an error."
